963a84c5f83f47d71b7fc54010358fa5d43a9c4a
Surajkumar40 <surajk74643@gmail.com>
2025-07-09 05:26:16 +0000

    # File managing properly

---
+++ 01_Basics/02_dataypes.js
@@ -0,0 +1,20 @@
+"use Strict"; // use all JS code as newer version
+// alert(3 + 3); // we use nodejs not a browser
+
+let isLoggedIn = true
+let state;
+
+// numbers => 2 to the power of 53
+// bigint => use for long number
+// string => "" , ''
+// boolean => true/false
+// null => standalone value
+// undefined => not defined
+// symbol => uniqueness
+
+//object
+
+console.log(typeof "Suraj");// string
+console.log(typeof null);// object
+console.log(typeof undefined);// undefined
+
